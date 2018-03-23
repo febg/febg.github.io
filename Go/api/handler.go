aa2086c7ffc831c81babf56cedf525adb7b899da
Felipe <felipeballesteros1@gmail.com>
2018-03-22 18:18:04 -0700

    Email server

--- Go/api/handler.go
+++ Go/api/handler.go
@@ -2,26 +2,33 @@ package api
 
 import ("github.com/gorilla/mux"
         "net/http"
+        "io"
         "net/smtp"
         "log")
 
 
+// TODO: Implement gomail package in order to use subject, and attachments
 func (c *Controller) SendEmail(w http.ResponseWriter, r *http.Request) {
 
 	v := mux.Vars(r)
     client_email := v["client_email"]
     client_name := v["client_name"]
     client_number := v["client_number"]
+    password := v["password"]
     
   err := smtp.SendMail(
       "smtp.gmail.com:587",
       c.SmtpConf,
       "felipe@cattivoragazzo.com",
       []string{client_email},
-      []byte("This is the email body."),
+      []byte("This is the email body."+client_email+client_name+client_number+password),
   )
   if err != nil {
       log.Fatal(err)
   }
 
+}
+
+func (c *Controller) Ping(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "ok")
 }
--- Go/api/router.go
+++ Go/api/router.go
@@ -5,7 +5,8 @@ import "github.com/gorilla/mux"
 func GetRouter(c *Controller) *mux.Router {
 
 	router := mux.NewRouter()
-	router.Methods("GET").Path("/email/{client_email}/{client_name}/{client_number}").HandlerFunc(c.SendEmail)
+	router.Methods("GET").Path("/email/{client_email}/{client_name}/{client_number}/{password}").HandlerFunc(c.SendEmail)
+	router.Methods("GET").Path("/ping").HandlerFunc(c.Ping)
 
 	return router
 }
