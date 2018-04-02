95f7643e48e0356e273133bb2ee635beeb522cf3
Felipe <felipeballesteros1@gmail.com>
2018-04-01 21:48:08 -0700

    Updated BespokeEmail

--- Go/api/handler.go
+++ Go/api/handler.go
@@ -1,80 +1,75 @@
 package api
 
-import ("github.com/gorilla/mux"
-        "net/http"
-        "io"
-        
-        "gopkg.in/gomail.v2"
-        )
+import (
+	"io"
+	"net/http"
 
+	"github.com/gorilla/mux"
+
+	"gopkg.in/gomail.v2"
+)
 
 // TODO: Implement gomail package in order to use subject, and attachments
 func (c *Controller) SendEmail(w http.ResponseWriter, r *http.Request) {
 
 	v := mux.Vars(r)
-    client_email := v["client_email"]
-    // client_name := v["client_name"]
-    // client_number := v["client_number"]
-     password := v["password"]
-    
-    m := gomail.NewMessage()
-    if (password == "Bespoke4Life"){
-        m.SetBody("text/html", `<body style ="text-align:center">Thank you for RSVPing for our VIP Event celebrating bespoke suiting. <br /><br />
+	client_email := v["client_email"]
+	// client_name := v["client_name"]
+	// client_number := v["client_number"]
+	password := v["password"]
 
-            You have been added to the guest list and will receive
-            
-            a follow-up email with additional details in the days before
-            
-            revealing the location and libation selection.<br /><br/>
-            
+	m := gomail.NewMessage()
+	if password == "Bespoke4Life" {
+		m.SetBody("text/html", `<body style ="text-align:center">
+Thank you for RSVPing for our VIP Event celebrating bespoke suiting. <br /><br />
+
+            You have been added to the guest list and will receive a reminder email the night before the event.
+            Looking forward to hosting you!.<br /><br/>
+
             If you have any questions regarding the event, Cattivo Ragazzo, or the Master Tailors,
-            please do not hesitate to contact helen@cattivoragazzo.com
-            
-           </body>`)
-    }else{
-        m.SetBody("text/html", `<body style ="text-align:center">Thank you for booking a consultation for our VIP Event celebrating bespoke suiting. <br /><br />
+            please do not hesitate to contact helen@cattivoragazzo.com 
 
-            Dino Minichiello and the Cattivo Ragazzo team look forward to welcoming you 
-            
-            to our atelier for your personalized appointment 
-            
+           </body>`)
+	} else {
+		m.SetBody("text/html", `<body style ="text-align:center">Thank you for booking a consultation for our VIP Event celebrating bespoke suiting. <br /><br />
+
+            Dino Minichiello and the Cattivo Ragazzo team look forward to welcoming you
+
+            to our atelier for your personalized appointment
+
             with Master Tailors Verolo & Lemmi.<br /><br/>
-            
+
             If you have any questions in advance of setting your appointment,
 Cattivo Ragazzo, or the Master Tailors,
 please do not hesitate to contact tania@cattivoragazzo.com.
-            
-           </body>`) }
-   
-    m.SetHeader("From", "Helen@cattivoragazzo.com")
-    m.SetHeader("To", client_email)
-    m.SetHeader("Subject", "Confirmation of Cattivo Ragazzo VIP event RSVP")
-    
- 
 
-    d := gomail.NewPlainDialer("smtp.gmail.com", 587, "Helen@cattivoragazzo.com", "Victoria1122!!")
+           </body>`)
+	}
 
-    // Send the email to Bob, Cora and Dan.
-    if err := d.DialAndSend(m); err != nil {
-        panic(err)
-    }
+	m.SetHeader("From", "Helen@cattivoragazzo.com")
+	m.SetHeader("To", client_email)
+	m.SetHeader("Subject", "Confirmation of Cattivo Ragazzo VIP event RSVP")
 
+	d := gomail.NewPlainDialer("smtp.gmail.com", 587, "Helen@cattivoragazzo.com", "Victoria1122!!")
 
-   
+	// Send the email to Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
 
-//   err := smtp.SendMail(
-//       "smtp.gmail.com:587",
-//       c.SmtpConf,
-//       "felipe@cattivoragazzo.com",
-//       []string{client_email},
-//       []byte("This is the email body."+client_email+client_name+client_number+password),
-//   )
-//   if err != nil {
-//       log.Fatal(err)
-//   }
+	//   err := smtp.SendMail(
+	//       "smtp.gmail.com:587",
+	//       c.SmtpConf,
+	//       "felipe@cattivoragazzo.com",
+	//       []string{client_email},
+	//       []byte("This is the email body."+client_email+client_name+client_number+password),
+	//   )
+	//   if err != nil {
+	//       log.Fatal(err)
+	//   }
 
 }
 
 func (c *Controller) Ping(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "ok")
+	io.WriteString(w, "ok")
 }
